3ac5983d70dc6cb41e3a1b73917b3743ddab605b
Valery <wogl@mail.ru>
2022-12-05 12:52:56 +0300

    complete

--- 50zadacha/Program.cs
+++ 50zadacha/Program.cs
@@ -9,87 +9,120 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-// Console.WriteLine("введите размерность двумерного массима m x n");
-// Console.Write("введите m(строку): ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.Write("введите n(столбец): ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// int [,] matrix = new int[m, n];
+Repeat2:
+Console.WriteLine("1 - поиск элемента по номеру позиции, 2 - поиск по индексу:");
+int answer = Convert.ToInt32(Console.ReadLine());
 
-
-// for(int i = 0; i < m; i++)
-// {
-//     for (int j = 0; j < n; j++)
-//     {
-//         matrix[i, j] = new Random().Next(1, 10); // [1, 10)
-//         Console.Write(matrix[i, j] + " ");
-//     }
-//     Console.WriteLine();
-// }
-
-// Repeat:
-// Console.WriteLine("Введите позицию элемента. Начинается с [1, 1]: ");
-// Console.WriteLine("Введите M");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите N");
-// int b = Convert.ToInt32(Console.ReadLine());
-
-// if (a > 0 && b > 0)
-// {
-//     if (a > m || b > n)
-//         Console.WriteLine("такого числа нет");
-//     else
-//     {
-
-//         Console.WriteLine(matrix[a - 1, b - 1]);
-//     }
-// }
-// else
-// {
-//     Console.WriteLine("вы ввели координаты неправильно, повторите попытку:");
-//     goto Repeat;
-// }
-
-
-
-
-Console.WriteLine("введите размерность двумерного массима m x n");
-Console.Write("введите m(строку): ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите n(столбец): ");
-int n = Convert.ToInt32(Console.ReadLine());
-int [,] matrix = new int[m, n];
-Console.WriteLine("Двумерный массив");
-for(int i = 0; i < m; i++)
+if (answer == 1 || answer == 2)
 {
-    for (int j = 0; j < n; j++)
+    if (answer == 2)
     {
-        matrix[i, j] = new Random().Next(1, 10); // [1, 10)
-        Console.Write(matrix[i, j] + " ");
-    }
+        Console.WriteLine("введите размерность двумерного массима m x n");
+        Console.Write("введите m(строку): ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.Write("введите n(столбец): ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int [,] matrix = new int[m, n];
+
+
+        for(int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = new Random().Next(1, 10); // [1, 10)
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     Console.WriteLine();
-}
+    Repeat:
+    Console.WriteLine("Введите позицию элемента. Начинается с [1, 1]: ");
+    Console.WriteLine("Введите M(строку)");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите N(столбец)");
+    int b = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine();
+        if (a > 0 && b > 0)
+        {
+            if (a > m || b > n)
+                Console.WriteLine("такого числа нет");
+            else
+                {
 
-int q = matrix.GetLength(0) * matrix.GetLength(1);
-int[] arr = new int[q];
-int k = 0;
-Console.WriteLine("преобразовали в одномерный массив");
-for(int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        arr[k] = matrix[i, j];
-        // Console.Write($"{arr[k]}" + " "); // вывод одномерного массива
-        k++;
+                    Console.WriteLine($"ваше число с индексом " + (a, b) + " это: " + (matrix[a - 1, b - 1]));
+                }
+        }
+        else
+        {
+            Console.WriteLine("вы ввели координаты неправильно, повторите попытку:");
+            goto Repeat;
+        }
+
     }
+
+
+
+    if (answer == 1)
+    {
+        Console.WriteLine("введите размерность двумерного массима m x n");
+        Console.Write("введите m(строку): ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.Write("введите n(столбец): ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int [,] matrix = new int[m, n];
+        Console.WriteLine("Двумерный массив");
+
+        for(int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = new Random().Next(1, 10);
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        int q = matrix.GetLength(0) * matrix.GetLength(1);
+        int[] arr = new int[q];
+        int k = 0;
+        // Console.WriteLine("преобразовали в одномерный массив");
+
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                arr[k] = matrix[i, j];
+                // Console.Write($"{arr[k]}" + " "); // вывод одномерного массива
+                k++;
+            }
     
+        }
+
+        Console.WriteLine("Введите позицию элемента начиная с 1.");
+        int position = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("---------------------------------------------");
+        if (position <= q && position > 0)
+        {
+            Console.WriteLine($"Число на {position} позиции это: {arr[position - 1]}");
+        }
+        else if (position > q)
+        {
+            Console.WriteLine($"Максимальная позиция нашего массива = {q}.");
+            Console.WriteLine("Такого элемента не существует");
+        }
+        else if (position < 1)
+        {
+            Console.WriteLine("Позиция начинается с 1, такого элемента нет");
+        }
+        Console.WriteLine("---------------------------------------------");
+    }
 }
-
-Console.WriteLine("Введите позицию элемента начиная с 1.");
-int position = Convert.ToInt32(Console.ReadLine());
- Console.WriteLine($"Число на {position} позиции это {arr[position - 1]}");
-
+else
+{
+    Console.WriteLine("Вы ввели неправильно, повторите попытку");
+    goto Repeat2;
+}
 
 
